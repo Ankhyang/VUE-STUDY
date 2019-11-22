c8fd02c8825aa11916a1530d73ed9942df393a12
Ankhyang <ankh3690@163.com>
2019-11-22 16:49:07 +0800

    vue开发的数字加减

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
   <div id="app">
-    <el-button>新增</el-button>
+    <p>我是数字 {{count}}, 我是{{evenOrOdd}}</p>
+    <el-button @click="increment" type="primary">新增</el-button>
+    <el-button @click="decrement" type="danger">减少</el-button>
+    <el-button @click="incrementAsync" type="warning" plain >异步增加</el-button>
+    <el-button @click="incrementIfOdd" type="success">偶数增加</el-button>
   </div>
 </template>
 
 <script>
+import {mapState, mapActions, mapGetters} from 'vuex'
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {
+    ...mapGetters(['evenOrOdd']),
+    ...mapState(['count'])
+  },
+  methods: {
+    ...mapActions(['increment', 'decrement', 'incrementAsync', 'incrementIfOdd'])
+    // increment () {
+    //   this.$store.dispatch('increment')
+    // },
+    // decrement () {
+    //   this.$store.dispatch('decrement')
+    // },
+    // incrementAsync () {
+    //   this.$store.dispatch('incrementAsync')
+    // },
+    // incrementIfOdd () {
+    //   this.$store.dispatch('incrementIfOdd')
+    // }
+  }
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -28,12 +28,22 @@ const actions = {
   },
   decrement ({commit}) {
     commit('DECREMENT')
+  },
+  incrementAsync ({commit}) {
+    setTimeout(() => {
+      commit('INCREMENT')
+    }, 1000)
+  },
+  incrementIfOdd ({commit}) {
+    if (state.count % 2 === 0) {
+      commit('INCREMENT')
+    }
   }
 }
 
 export default new Vuex.Store({
-  getters,
-  actions,
-  mutations,
-  state
+  getters, // 包含多个计算属性的对象
+  actions, // 包含多个动作的对象
+  mutations, // 包含多个触发state的
+  state // 状态对象
 })
