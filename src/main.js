27c1c6194bb1e6d9050e6a9e4649455baecda3d0
Ankhyang <ankh3690@163.com>
2019-12-23 17:20:38 +0800

    promise对象

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
@@ -13,8 +11,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
-  template: '<App/>',
-  store
+  template: '<App/>'
 })
